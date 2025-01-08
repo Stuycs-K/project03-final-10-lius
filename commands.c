a0b049b02a7eda7165f780be217879371040f79f
somebody <81702427+sOm38Ody@users.noreply.github.com>
2025-01-08 00:41:29 -0500

    added basic code for mp3 file playing

--- commands.c
+++ commands.c
@@ -4,6 +4,30 @@
 #include "library.h"
 
 void play_song() {
+  char song[256];
+  printf("Enter song to play: ");
+  if (fgets(song, sizeof(song), stdin) == NULL) {
+    exit(1);
+  }
+  //need to get mp3 file associated w/ inputted song name
+  //if no mp3 file --> MP3 file does not exist
+  
+  pid_t pid = fork();
+  if (pid < 0) {
+    perror("fork fail");
+    exit(1);
+  }
+  if (pid == 0) {
+    // prepare argument list for exec
+    char *args[] = {"mpg123", song, NULL};
+
+    // execute mpg123 program to play the MP3 file
+    execvp("mpg123", args);
+  }
+  else { //parent
+    printf("Playing song...\n");
+    wait(NULL);
+  }
 
 }
 
@@ -15,6 +39,7 @@ void add_song(struct song_node ** library) {
   printf("2 - Automatic\n");
 
   if (fgets(input, sizeof(input), stdin) == NULL) {
+    //return;
     exit(1);
   }
   if (strcmp(input, "1") == 0) {
